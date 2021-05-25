266055c169f4b53d6c85cf853dd9e6c0845febb8
Tom Powell <thomastruittpowell@gmail.com>
2021-05-25 08:31:26 -0400

    improved build cli
    fixed issues with build.sh

--- src/build.sh
+++ src/build.sh
@@ -1,8 +1,13 @@
 g++ --version | head -n 1 
 # grab the last version number from file
-#lastnum=cat ../dat/version.txt
-lastnum=$(<../dat/version.txt)
-newnum=$((lastnum +1))
+lastnum=$(cat "../dat/version.txt")
+newnum=$((lastnum +1)) #increment
+echo "Build number $newnum"
 fname="rocket$newnum.o"
-rm ../bin/rocket$lastnum.o
-g++ -std=c++17 -o ../bin/$fname -B . main.cpp
+fp="../bin/rocket$lastnum.o"
+rm $fp
+g++ -std=c++17 -o ../bin/$fname -B . main.cpp # compile under new filename
+# if the compile was successful, remove old file
+if test -f "../bin/rocket$newnum.o"; then
+    echo $newnum > ../dat/version.txt #update the version file
+fi
